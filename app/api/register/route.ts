8617f0304660930e27af434caadca57c4d2655b9
Stephan Moerman <stephan.moerman@viewnary.com>
2023-08-25 13:39:55 +0200

    Added email/password registration functionality

---
+++ app/api/register/route.ts
@@ -0,0 +1,28 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/app/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { email, name, password } = body;
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json({
+      error: "Internal server error",
+      status: 500,
+    });
+  }
+}
