938e3a12a9cafabf067cc349f7188545f937130a
Stephan Moerman <stephan.moerman@viewnary.com>
2023-08-25 18:29:41 +0200

    Finished create listing process

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -32,13 +32,39 @@ const Input: React.FC<InputProps> = ({
           className="text-neutral-700 absolute top-5 left-2"
         />
       )}
-      <input
-        id={id}
-        disabled={disabled}
-        {...register(id, { required })}
-        placeholder=" "
-        type={type}
-        className={`
+      {type === "textarea" ? (
+        <textarea
+          id={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          placeholder=" "
+          className={`
+          peer
+          w-full
+          p-4
+          pt-6
+          font-light
+          bg-white
+          border-2
+          rounded-md
+          outline-none
+          transition
+          disabled:opacity-70
+          disabled:cursor-not-allowed
+          ${formatPrice ? "pl-9" : "pl-4"}
+          ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+          ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
+          `}
+          rows={5}
+        />
+      ) : (
+        <input
+          id={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          placeholder=" "
+          type={type}
+          className={`
           peer
           w-full
           p-4
@@ -55,7 +81,8 @@ const Input: React.FC<InputProps> = ({
           ${errors[id] ? "border-rose-500" : "border-neutral-300"}
           ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
         `}
-      />
+        />
+      )}
       <label
         className={`
         absolute
