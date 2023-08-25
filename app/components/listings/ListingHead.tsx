52d50808dc2a33ca2cbbc75cb9bfbcb28b66da0c
Stephan Moerman <stephan.moerman@viewnary.com>
2023-08-25 21:44:28 +0200

    listing view - image, data, description and map added

---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+
+import Heading from "../modals/Heading";
+import AddToFavoriteButton from "./AddToFavoriteButton";
+
+interface ListingHeadProps {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({
+  title,
+  locationValue,
+  imageSrc,
+  id,
+  currentUser,
+}) => {
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+      <div className="w-full h-[40vh] overflow-hidden rounded-xl relative">
+        <Image
+          src={imageSrc}
+          alt={`${title} image`}
+          fill
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <AddToFavoriteButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListingHead;
