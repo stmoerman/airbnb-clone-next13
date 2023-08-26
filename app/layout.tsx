0f9fac3c0c243ecb5adae5d7e249dab9d92701e9
Stephan Moerman <stephan.moerman@viewnary.com>
2023-08-26 23:08:57 +0200

    Added filtering and search to find listings

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Navbar from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
 import LoginModal from "./components/modals/LoginModal";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
+
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 
@@ -29,6 +31,7 @@ export default async function RootLayout({
       <body className={nunito.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
